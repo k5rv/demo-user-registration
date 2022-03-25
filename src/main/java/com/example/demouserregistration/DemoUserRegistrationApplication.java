9e6ac289cb8f413c5ae0159c57276fdcc7f63314
konstantin <k.saraev@gmail.com>
2022-03-25 08:29:03 +0200

    Gathered project initial files

---
+++ src/main/java/com/example/demouserregistration/DemoUserRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demouserregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoUserRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoUserRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demouserregistration/DemoUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demouserregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoUserRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
