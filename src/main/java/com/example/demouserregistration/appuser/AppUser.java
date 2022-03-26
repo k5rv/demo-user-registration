a19cbc54b3db68f92254514b12024dbd553ce4c6
konstantin <k.saraev@gmail.com>
2022-03-26 18:13:04 +0200

    Updated UserApp model and naming

--- src/main/java/com/example/demouserregistration/appuser/AppUser.java
+++ src/main/java/com/example/demouserregistration/appuser/AppUser.java
@@ -20,37 +20,31 @@ import java.util.Objects;
 public class AppUser implements UserDetails {
 
   @SequenceGenerator(
-      name = "student_sequence",
-      sequenceName = "student_sequence",
+      name = "app_user_sequence",
+      sequenceName = "app_user_sequence",
       allocationSize = 1)
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "app_user_sequence")
   @Id
   private Long id;
 
-  private String name;
-  private String username;
+  private String firstName;
+  private String lastName;
   private String email;
   private String password;
+
   @Enumerated(EnumType.STRING)
   private AppUserRole appUserRole;
-  private Boolean locked;
-  private Boolean enabled;
+
+  private Boolean locked = false;
+  private Boolean enabled = false;
 
   public AppUser(
-      String name,
-      String username,
-      String email,
-      String password,
-      AppUserRole appUserRole,
-      Boolean locked,
-      Boolean enabled) {
-    this.name = name;
-    this.username = username;
+      String firstName, String lastName, String email, String password, AppUserRole appUserRole) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.email = email;
     this.password = password;
     this.appUserRole = appUserRole;
-    this.locked = locked;
-    this.enabled = enabled;
   }
 
   @Override
@@ -66,7 +60,7 @@ public class AppUser implements UserDetails {
 
   @Override
   public String getUsername() {
-    return username;
+    return email;
   }
 
   @Override
